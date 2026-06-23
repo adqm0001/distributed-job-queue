af4aa05fee0150f68e309b13b7cfc9c2d9d9c752
adqm0001 <adam.benmoussa04@gmail.com>
2026-06-22 23:22:40 -0400

    Add scheduling policy interface and FIFO

---
+++ internal/policy/fifo.go
@@ -0,0 +1,20 @@
+package policy
+
+import "github.com/adqm0001/distributed-job-queue/internal/job"
+
+type FIFO struct {
+	jobs []*job.Job
+}
+
+func (fifo *FIFO) Add(j *job.Job) {
+	fifo.jobs = append(fifo.jobs, j)
+}
+
+func (fifo *FIFO) Next() *job.Job {
+	if len(fifo.jobs) == 0 {
+		return nil
+	}
+	next := fifo.jobs[0]
+	fifo.jobs = fifo.jobs[1:]
+	return next
+}
--- internal/policy/policy.go
+++ internal/policy/policy.go
@@ -1,1 +1,8 @@
 package policy
+
+import "github.com/adqm0001/distributed-job-queue/internal/job"
+
+type SchedulingPolicy interface {
+	Add(j *job.Job)
+	Next() *job.Job
+}
